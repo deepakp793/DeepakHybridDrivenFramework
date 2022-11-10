ab48221d527ee9a9577a11fc1bd889408685ad7e
Deepak <deepak.patil793@gmail.com>
2022-11-10 08:45:40 +0530

    Test Confilicts

--- src/utility/A.java
+++ src/utility/A.java
@@ -4,7 +4,7 @@ public class A {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Test Confilcts1");
+		System.out.println("Test Confilcts1-Conflict Testing");
 		System.out.println("Test Conflicts2");
 	}
 
